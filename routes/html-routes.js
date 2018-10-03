f9b2ab6bd909dd4764e7b8669ddead649f907949
Choleslaw <41842273+kymiddleton@users.noreply.github.com>
2018-10-03 14:35:52 -0400

    adding initial project files for team collaboration

---
+++ routes/html-routes.js
@@ -0,0 +1,11 @@
+module.exports = function (app) {
+
+    app.get('/', function (req, res) {
+      console.log('HIT THE HOME ROUTE');
+      res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+  
+    app.get('*', function (req, res) {
+      res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+  };
