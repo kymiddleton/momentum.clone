45e6bc81268de91b9f84edc5f9943077b00ab10a
gtrmonkey <gtrmonkey@gmail.com>
2018-10-25 19:52:14 -0400

    fixed the clock up dateing

--- public/js/clock.js
+++ public/js/clock.js
@@ -1,27 +1,15 @@
-/**
- * Function of the Clock
- * startTime function is for 24hour time
- * appendZero adds a Zero to both clocks
- * Base12hour function controls the Am Pm on twelveTime
- * TwelveTime funtion is for 12hour time
- * Toggling Between Milc(MilitaryClock) and Rc(Regular), To keep them centered
- * Click functions covers on the toggle between startTime and twelveTime
- * Also controls the Am and Pm transtion to fade out by Jquery fade out function and a Set Invertnal time. 
- * 
- * 
- * 
- */
 
-
+/**The major function that holds all the interworking of the clock. */
 
 $(function() {
+  
     startTime();
-
+   /** appendZero adds a Zero to both clocks */
     function  appendZero (input) {
         if (input < 10) {input = "0" + input};  
         return input;
     };
-
+/** startTime function is for 24hour time*/ 
     function startTime() {
         var today = new Date();
         var hour = today.getHours();
@@ -31,10 +19,9 @@ $(function() {
         minute = appendZero(minute);
         
         document.getElementById('default-clock').innerHTML = hour + ":" + minute;
-        $("#default-clock").addClass("milc");
-        $("#default-clock").removeClass("rc");
+        setTimeout(startTime, 1000);
     };
-
+    /**Base12hour function controls the Am Pm on twelveTime */
     function base12Hour (hour, minute){
         console.log("I'm running")
         if(hour > 12){
@@ -51,7 +38,7 @@ $(function() {
         
         return [hour, minute];
     };
-
+    /**TwelveTime funtion is for 12hour time*/
     function twelveTime (){
         var today = new Date();
         var hour = today.getHours();
@@ -65,24 +52,33 @@ $(function() {
         hour = tempArray[0];
         minute = tempArray[1];
 
-        document.getElementById('default-clock').innerHTML = hour + ":" + minute;
-        $("#default-clock").addClass("rc");
-        $("#default-clock").removeClass("milc")
-        
+        document.getElementById('clockrc').innerHTML = hour + ":" + minute;
+        setTimeout(twelveTime, 1000);
+        return twelveTime;
         
     };
+    /**Toggling Between Milc(MilitaryClock) and Rc(Regular), To keep them centered
+     * Click functions covers on the toggle between startTime and twelveTime
+     * Also controls the Am and Pm transtion to fade out by Jquery fade out function and a Set Invertnal time
+    */
     toggle = false;
-    $('#default-clock').click(function(e) {
+    $('#clockc').click(function(e) {
         toggle = !toggle;
         if (!toggle) {
             startTime();
             $('#ampm').css('opacity', 1);
             $('#ampm').hide();
+            $('#clockrc').hide();
+            $('#default-clock').show();
+
+            
       console.log("rc" +toggle);
         } else {
             twelveTime();
             $('#ampm').show();
-            setInterval(function() {$('#ampm').fadeOut(2000);}, 8000);
+            $('#default-clock').hide();
+            $('#clockrc').show();
+           setInterval(function() {$('#ampm').fadeOut(2000);}, 8000);
             console.log("mc" +toggle);
         }
     });
